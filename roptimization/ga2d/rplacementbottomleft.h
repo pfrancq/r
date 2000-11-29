4b9a6649a10b5016a2b63252d07c27d74e65ad6b
pfrancq <pfrancq@bc43a8d9-cd2b-0410-a3c8-d37dcb895449>
2000-11-29 16:15:06 +0000

    - Initalisation of result in RPlacementHeuristic.

--- roptimization/ga2d/rplacementbottomleft.h
+++ roptimization/ga2d/rplacementbottomleft.h
@@ -31,41 +31,76 @@
 
 
 
-//---------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 #ifndef RPlacementBottomLeftH
 #define RPlacementBottomLeftH
 
 
-//---------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // include files for Rainbow
 #include "rplacementheuristic.h"
 
 
-//---------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 namespace RGA{
-//---------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 
-//---------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 /**
 * The RPlacementHeuristic class provides an abstract class for placement
 * heuristics.
+*
+* This heuristic hanbdles objects "line after line". The first object is place
+* in bottom-left part of the rectangle representing the limits. After that, the
+* heuristic try to place so much objects as possible to the right of the first
+* object. When it is not more possible, the next object is place top of the
+* first one. The heuristic try then to put the rest of the objects to the right
+* and so on.
 * @author Pascal Francq
 * @short Generic Placement Heuristic class.
 */
 class RPlacementBottomLeft : public RPlacementHeuristic
 {
+	/**
+	* Position where to place the next object.
+	*/
 	RPoint Actual;
+
+	/**
+	* Where the next line begin.
+	*/
 	RPoint Last;
+
 public:
+
+	/**
+	* Construct the bottom-left heuristic.
+	* @param maxobjs		Maximal number of objects to place.
+	*/
 	RPlacementBottomLeft(unsigned int maxobjs);
+
+	/**
+	* Initialize the heuristic.
+	* @param limits		Limits for the placement.
+	* @param grid			Pointer to the grid.
+	* @param objs			Pointer to the objects.
+	* @param infos			Pointer to the geometric information.
+	* @param nbobjs		Number of objects to place.
+	*/
 	virtual void Init(RPoint &limits,RGrid *grid,RObj2D** objs,RGeoInfo **infos,unsigned int nbobjs);
+
+	/**
+	* Place the next object for a specific information.
+	* @return		The function returns true if the object can be placed with the
+	*					current orientation.
+	*/
 	virtual bool NextObjectOri(void);
 };
 
 
-}//------- End of namespace RGA ---------------------------------------------
+}  //------- End of namespace RGA ---------------------------------------------
 
 
-//---------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 #endif
