6220a216ffb767a94ea9980ebfcb7c068f7e609e
pfrancq <pfrancq@bc43a8d9-cd2b-0410-a3c8-d37dcb895449>
2001-06-26 15:21:56 +0000

    - Skip 'limits.h'.

--- rcore/tools/random.cpp
+++ rcore/tools/random.cpp
@@ -33,19 +33,20 @@
 //-----------------------------------------------------------------------------
 #include <rmath/random.h>
 using namespace RMath;
-#if !defined(LIMITS_H_)
-  #include <limits.h>
-# define LIMITS_H_
-#endif // LIMITS_H_
+
+
+//-----------------------------------------------------------------------------
+// Defines
+#define INT_MAX 0xFFFFFFFF
 
 
 //-----------------------------------------------------------------------------
 // General Variables
 static const int a=16807;
-static const int q=INT_MAX/a;                //  cf limits.h; INT_MAX == LONG_MAX in C++
-static const int r=INT_MAX%a;
-static const double Minv=1.0/INT_MAX;
-static const int NDIV=1+((INT_MAX-1)/32);
+static const int q=0xFFFFFFFF/a;                //  cf limits.h; INT_MAX == LONG_MAX in C++
+static const int r=0xFFFFFFFF%a;
+static const double Minv=1.0/0xFFFFFFFF;
+static const int NDIV=1+((0xFFFFFFFF-1)/32);
 static const double MaxR=1.0-1.2e-7;         //  maximum value to return
 static const int M1=2147483563;
 static const int a1=16807;
