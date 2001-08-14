a03b439835be9c362390678536480298fcadbc45
pfrancq <pfrancq@bc43a8d9-cd2b-0410-a3c8-d37dcb895449>
2001-08-14 16:40:20 +0000

    - Implementation header file.

---
+++ roptimization/ga/rgaimplementation.h
@@ -0,0 +1,60 @@
+/*
+
+	R Project Library
+
+	RGAImplementation.h
+
+	Genetic Algorithms - Inline Implementations
+
+	(C) 1998-2001 by P. Francq.
+
+	Version $Revision$
+
+	Last Modify: $Date$
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library, as a file COPYING.LIB; if not, write
+	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+#ifndef RGAImplementationH
+#define RGAImplementationH
+
+
+//-----------------------------------------------------------------------------
+// include files for R Project
+#include <rga/rga.h>
+
+
+
+//-----------------------------------------------------------------------------
+namespace RGA{
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Definitions of templates
+#include <rga/rfitness.hh>
+#include <rga/rinst.hh>
+#include <rga/rchromo.hh>
+
+
+}  //------- End of namespace RGA ---------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+#endif
