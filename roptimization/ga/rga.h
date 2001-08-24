2e83bc1b5a2d8f653c58991ad2462f8b9761c905
pfrancq <pfrancq@bc43a8d9-cd2b-0410-a3c8-d37dcb895449>
2001-08-24 14:17:12 +0000

    - Nothing.

--- roptimization/ga/rga.h
+++ roptimization/ga/rga.h
@@ -57,7 +57,7 @@ using namespace RIO;
 using namespace RMath;
 
 
-
+//-----------------------------------------------------------------------------
 /**
 * \namespace RGA
 * \brief Generic Genetic Algorithm Classes.
