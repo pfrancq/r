e71f66c2013254fd2fe3594d942b4e3e691031e6
pfrancq <pfrancq@bc43a8d9-cd2b-0410-a3c8-d37dcb895449>
2004-07-23 16:33:39 +0000

    - Add a class 'RDownload' based on the CURL library to download files.

---
+++ rcore/tools/rdownload.cpp
@@ -0,0 +1,139 @@
+/*
+
+	R Research Project
+
+	RDownload.cpp
+
+		Class to download files (using the CURL library) - Implementation.
+
+	Copyright 2004 by the Université Libre de Bruxelles.
+
+	Authors:
+		Valery Vandaele (vavdaele@ulb.ac.be)
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library, as a file COPYING.LIB; if not, write
+	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//------------------------------------------------------------------------------
+// include files for ANSI C/C++
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+
+
+//------------------------------------------------------------------------------
+// include files for R library
+#include <rstd/rdownload.h>
+using namespace R;
+using namespace std;
+
+
+
+//------------------------------------------------------------------------------
+//
+// Internal function and structure
+//
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+struct DwnFile
+{
+	const char* filename;
+	FILE *stream;
+};
+
+
+//------------------------------------------------------------------------------
+int WriteTmpFile(void* buffer, size_t size, size_t nmemb, void* stream)
+{
+	struct DwnFile* out=(struct DwnFile*)stream;
+	if(out && !out->stream)
+	{
+		/* open file for writing */
+		out->stream=fopen(out->filename, "wb");
+		if(!out->stream)
+		return(-1);
+	}
+	return(fwrite(buffer, size, nmemb, out->stream)); 
+}
+
+
+
+//------------------------------------------------------------------------------
+//
+// class RDownload
+//
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+RDownload::RDownload(void) throw(std::bad_alloc)
+{
+	Lib = curl_easy_init();
+}
+
+
+//------------------------------------------------------------------------------
+void RDownload::Download(const char* URL,RString& tmpFile) throw(RException)
+{
+	
+	struct DwnFile tmpfile;
+	int err;
+	char filename[L_tmpnam+1];
+	// Fill structure -> 
+	//find a local temporary file if tmpFile not specified else use the one specified
+	if(tmpFile.IsEmpty())
+	{
+		tmpnam(filename);
+		tmpfile.filename=filename;
+		tmpfile.stream=0;
+		tmpFile=tmpfile.filename;
+	}
+	else 
+	{
+		tmpfile.stream=0;
+		tmpfile.filename=tmpFile.Latin1();
+	}
+
+	// Download the file
+	curl_easy_setopt(Lib, CURLOPT_URL, URL);
+	curl_easy_setopt(Lib, CURLOPT_WRITEFUNCTION,WriteTmpFile);
+	curl_easy_setopt(Lib, CURLOPT_FILE, &tmpfile);
+	//curl_easy_setopt(Lib, CURLOPT_CONNECTTIMEOUT,120);
+	//curl_easy_setopt(Lib, CURLOPT_TIMEOUT,120);
+	
+	err=curl_easy_perform(Lib);
+	if(tmpfile.stream)
+		fclose(tmpfile.stream);
+	if(err)
+		throw RException(RString("Can't download url : ")+ URL);
+}
+
+
+//------------------------------------------------------------------------------
+void RDownload::Delete(RString& tmpFile) throw(RException)
+{
+	remove(tmpFile);
+}
+
+
+//------------------------------------------------------------------------------
+RDownload::~RDownload(void)
+{
+	curl_easy_cleanup(Lib);
+}
---
+++ rcore/tools/rdownload.h
@@ -0,0 +1,109 @@
+/*
+
+	R Research Project
+
+	RDownload.h
+
+	Class to download files (using the CURL library) - Header.
+
+	Copyright 2004 by the Université Libre de Bruxelles.
+
+	Authors:
+		Valery Vandaele (vavdaele@ulb.ac.be)
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
+
+	You should have received a copy of the GNU Library General Public
+	License along with this library, as a file COPYING.LIB; if not, write
+	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//------------------------------------------------------------------------------
+#ifndef RDownloadH
+#define RDownloadH
+
+
+//------------------------------------------------------------------------------
+// include files for ANSI C/C++
+#include <stdexcept>
+
+
+//------------------------------------------------------------------------------
+// include files for CURL
+#include <curl/curl.h>
+
+
+//------------------------------------------------------------------------------
+// include files for R library
+#include <rstd/rstring.h>
+
+
+
+//------------------------------------------------------------------------------
+namespace R{
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+/**
+* The RDownload class provides a representation for a downloader
+* using the CURL library.
+* @author Valery Vandaele 
+* @short Downloader using the CURL library.
+*/
+class RDownload
+{
+	/**
+	* Pointer to an instance of the library.
+	*/
+	CURL* Lib;
+
+public:
+
+	/**
+	* Construct the URL manager.
+	* @param path            Path to find the plugins.
+	* @param dlg             Should the dialog box be loaded.
+	*/
+	RDownload(void) throw(std::bad_alloc);
+
+	/**
+	* Download and store locally a document given by an URL. If the tmpFile contains 
+	* an url, the temporary document will be saved using this filename esle a file is 
+	* created with a random name
+	* @param URL            URL of the document.
+	* @param tmpFile        Temporary file created.
+	*/
+	virtual void Download(const char* URL,R::RString& tmpFile) throw(RException);
+
+	/**
+	* Delete a temporary copy of a file created by the manager. This method is
+	* only called if a temporary file was really created.
+	* @param tmpFile        Temporary file to delete.
+	*/
+	virtual void Delete(R::RString& tmpFile) throw(RException);
+
+	/**
+	* Destructor of the URL manager.
+	*/
+	virtual ~RDownload(void);
+};
+
+
+}  //-------- End of namespace R -----------------------------------------------
+
+
+//------------------------------------------------------------------------------
+#endif
