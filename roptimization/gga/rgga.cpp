96990b55de8880553abd9d570d29fc2be3222f71
pfrancq <pfrancq@bc43a8d9-cd2b-0410-a3c8-d37dcb895449>
2005-07-18 16:05:55 +0000

    - Create a separate library for the frontend.

--- roptimization/gga/rgga.cpp
+++ roptimization/gga/rgga.cpp
@@ -6,7 +6,7 @@
 
 	Grouping Genetic Algorithms - Implementation
 
-	Copyright 2001-2003 by the Université Libre de Bruxelles.
+	Copyright 2001-2003 by the Universitï¿½Libre de Bruxelles.
 
 	Authors:
 		Pascal Francq (pfrancq@ulb.ac.be).
