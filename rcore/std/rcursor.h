52bd8ad389ca38efbc5aebac511285b341daef9a
pfrancq <pfrancq@bc43a8d9-cd2b-0410-a3c8-d37dcb895449>
2007-07-06 11:12:16 +0000

    - Add a new class 'RChildCursor' to handle containers holding objects from a child class corresponding to the declared one.

--- rcore/std/rcursor.h
+++ rcore/std/rcursor.h
@@ -48,11 +48,10 @@ namespace R{
 
 //-----------------------------------------------------------------------------
 /**
-* @param C                   The class of the elements that are contained.
-*
-* This class represent a cursor to iterate a RContainer. The parameters are
-* those of the container. When an element is added or removed from the container
-* parsed by the cursor, the cursor is not valid anymore.
+* This class represent a cursor to iterate a RContainer.
+* @param C                   Class of the elements of the container.
+* \attention When an element is added or removed from the container parsed by
+* the cursor, the cursor is not valid anymore.
 *
 * Here is an example of a cursor used:
 * @code
@@ -154,13 +153,143 @@ public:
 	* @param max             Maximum position of the elements to iterate. If
 	*                        SIZE_MAX, iterate until the end of the container.
 	*/
-	template<bool b,bool o> void Set(const RContainer<C,b,o>& c,size_t min=0,size_t max=SIZE_MAX) { BasicCursor::Set(c,min,max);}
+	template<bool b,bool o> void Set(const RContainer<C,b,o>& c,size_t min=0,size_t max=SIZE_MAX) {BasicCursor::Set(c,min,max);}
 
 	/**
 	* Return the current element.
 	*/
 	inline C* operator()(void) const {return(reinterpret_cast<C*>(*Current));}
 
+};
+
+
+//-----------------------------------------------------------------------------
+/**
+* This class represent a child cursor to iterate a RContainer. It is used when
+* the container to iterate is supposed to handle objects from a given class but
+* the objects allocated are of a child class.
+* @param master              Class originally declared in the container. This
+*                            class must have at least one virtual method (for
+*                            example the destructor).
+* @param child               Class of the elements of the container. It must be
+*                            a child class of the master class.
+* \attention When an element is added or removed from the container parsed by
+* the cursor, the cursor is not valid anymore.
+* 
+* Here is an example of a cursor used:
+* @code
+* #include <rcontainer.h>
+* #include <rcursor.h>
+* using namespace R;
+*
+* class O
+* {
+* public:
+*    RString Name;
+*    O(const RString& name) : Name(name) {}
+*    int Compare(const O& o) const {return(Name.Compare(o.Name));}
+*    virtual ~O(void) {}    // Virtual destructor is necessary
+* };
+* 
+* class O2 : public O
+* {
+* public:
+*    int Id;
+*    O2(const RString& name,int id) : O(name), Id(id) {}
+* };
+* 
+* void Test(void)
+* {
+*    RContainer<O,true,true> Cont(20);
+*    Cont.InsertPtr(new O2("Obj1",1));
+*    Cont.InsertPtr(new O2("Obj2",2));
+*    Cont.InsertPtr(new O2("Obj3",3));
+*    RChildCursor<O,O2> Cur(Cont);
+*    for(Cur.Start();!Cur.End();Cur.Next())
+*       cout<<Cur()->Name<<" - "<<Cur()->Id<<endl;
+* }
+* @endcode
+*
+* @author Pascal Francq
+* @short Container Child Cursor.
+*/
+template<class master,class child>
+	class RChildCursor : public RCursor<master>
+{
+public:
+	
+	/**
+	* Construct the cursor.
+	*/
+	RChildCursor(void) : RCursor<master>() {}
+
+	/**
+	* Construct the cursor.
+	* @param src             Source container.
+	*/
+	RChildCursor(const RCursor<master>& src) : RCursor<master>(src) {}
+	
+	/**
+	* Construct the cursor.
+	* @param src             Source container.
+	*/
+	RChildCursor(const RChildCursor<master,child>& src) : RCursor<master>(src) {}
+	
+	/**
+	* Construct the cursor.
+	* @param c               Container to iterate.
+	* @param min             Minimum position of the elements to iterate.
+	* @param max             Maximum position of the elements to iterate. If
+	*                        SIZE_MAX, iterate until the end of the container.
+	*/
+	RChildCursor(const RContainer<master,true,true>& c,size_t min=0,size_t max=SIZE_MAX) : RCursor<master>(c,min,max) {}
+
+	/**
+	* Construct the cursor.
+	* @param c               Container to iterate.
+	* @param min             Minimum position of the elements to iterate.
+	* @param max             Maximum position of the elements to iterate. If
+	*                        SIZE_MAX, iterate until the end of the container.
+	*/
+	RChildCursor(const RContainer<master,false,true>& c,size_t min=0,size_t max=SIZE_MAX) : RCursor<master>(c,min,max) {}
+
+	/**
+	* Construct the cursor.
+	* @param c               Container to iterate.
+	* @param min             Minimum position of the elements to iterate.
+	* @param max             Maximum position of the elements to iterate. If
+	*                        SIZE_MAX, iterate until the end of the container.
+	*/
+	RChildCursor(const RContainer<master,true,false>& c,size_t min=0,size_t max=SIZE_MAX) : RCursor<master>(c,min,max) {}
+
+	/**
+	* Construct the cursor.
+	* @param c               Container to iterate.
+	* @param min             Minimum position of the elements to iterate.
+	* @param max             Maximum position of the elements to iterate. If
+	*                        SIZE_MAX, iterate until the end of the container.
+	*/
+	RChildCursor(const RContainer<master,false,false>& c,size_t min=0,size_t max=SIZE_MAX) : RCursor<master>(c,min,max) {}
+
+	/**
+	* Assignment operator using a "Cursor".
+	* @param src             Source container.
+	*/
+	RChildCursor<master,child>& operator=(const RChildCursor<master,child>& src) { return(static_cast<RChildCursor<master,child>&>(RCursor<master>::operator=(src)));}
+
+	/**
+	* Set the container.
+	* @param c               Container to iterate.
+	* @param min             Minimum position of the elements to iterate.
+	* @param max             Maximum position of the elements to iterate. If
+	*                        SIZE_MAX, iterate until the end of the container.
+	*/
+	template<bool b,bool o> void Set(const RContainer<master,b,o>& c,size_t min=0,size_t max=SIZE_MAX) {RCursor<master>::Set(c,min,max);}
+
+	/**
+	* Return the current element.
+	*/
+	inline child* operator()(void) const {return(dynamic_cast<child*>(RCursor<master>::operator()()));}
 };
 
 
