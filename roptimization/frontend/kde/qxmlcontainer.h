6e9197cf701e35b66d2e9b01534d90ca9b751bd1
pfrancq <pfrancq@bc43a8d9-cd2b-0410-a3c8-d37dcb895449>
2007-12-28 14:25:18 +0000

    - Documentation updated.

--- roptimization/frontend/kde/qxmlcontainer.h
+++ roptimization/frontend/kde/qxmlcontainer.h
@@ -1,25 +1,29 @@
 /*
+	R Project Library
 
 	QXMLContainer.h
 
 	Qt XML Debugger - Header.
 
-	(c) 2000-2001 by P. Francq.
+	Copyright 2000-2007 by the Université Libre de Bruxelles.
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	any later version.
+	Authors:
+		Pascal Francq (pfrancq@ulb.ac.be).
 
-	This program is distributed in the hope that it will be useful,
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Library General Public
+	License as published by the Free Software Foundation; either
+	version 2.0 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Library General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+	You should have received a copy of the GNU Library General Public
+	License along with this library, as a file COPYING.LIB; if not, write
+	to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+	Boston, MA  02111-1307  USA
 */
 
 
@@ -58,7 +62,7 @@ class QXMLContainer : public QListView, public RDebug
 	/**
 	* The array is used to manage the deepest of the XML structure.
 	*/
-	QListViewItem *Items[50];
+	QListViewItem* Items[50];
 
 public:
 
